d21362188f954dad8d5045f201a048d76ac5c52c
zhenglubiao <zlb@zhenglubiaodeMacBook-Pro.local>
2018-08-30 16:17:49 +0800

    Initial commit

---
+++ src/test/java/com/zlb/nettysocketiodemo/NettySocketioDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zlb.nettysocketiodemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NettySocketioDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
